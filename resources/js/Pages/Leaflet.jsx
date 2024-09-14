6b2b3bccc1dc8a7b62a0953a3228b53eccd9f70d
garrychrstn <garrychrstn@gmail.com>
2024-09-14 13:39:07 +0700

    add array for marker

--- resources/js/Pages/Leaflet.jsx
+++ resources/js/Pages/Leaflet.jsx
@@ -3,8 +3,12 @@ import { TileLayer } from 'react-leaflet/TileLayer'
 import { useMap } from 'react-leaflet/hooks'
 
 const Leaflet = () => {
-	
-    return ( 
+	let array_coordinate = [
+		{ name : 'pos1', la: '-7.588965925268359', long: '110.78278201288234'},
+		{ name : 'pos2', la: '-7.588473143069318', long: '110.78443027116379'},
+		{ name : 'pos3', la: '-7.588488508198645', long: '110.78355135581893'},
+	]
+    return (
         <>
 		<div id='map' className="m-auto w-4/5">
 		  <MapContainer center={[-7.604425054489175, 110.81664186804254]} zoom={20} className='h-10'>
