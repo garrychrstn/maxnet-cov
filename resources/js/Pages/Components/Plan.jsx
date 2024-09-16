760de8fbdaa2698a3ea8cdd7401660f3d2c440af
garrychrstn <garrychrstn@gmail.com>
2024-09-17 02:11:37 +0700

    add function to set packet initial price

--- resources/js/Pages/Components/Plan.jsx
+++ resources/js/Pages/Components/Plan.jsx
@@ -1,13 +1,21 @@
+import { useContext } from "react";
+import { RequestContext } from "../Home";
+
 const Plan = ({ packet }) => {
-    function pickPacket() {
+    const [custRequest, setCustRequest] = useContext(RequestContext)
+    function choosePacket(price) {
+        setCustRequest((request) => ({
+            ...request,
+            initial_price: price
+        }))
+        console.log(`price is set to : ${custRequest.initial_price}`);
         
     }
     return ( 
         <div className="box bg-prim rounded-md p-8 m-4 shrink">
             <h1 className="font-bold xl:text-5xl sm:text-3xl">{ packet.speed } Mbps</h1>
             <small>cocok untuk x pengguna</small>
-            <button onClick={ pickPacket(packet.price) } className="md:text-2xl sm:text-md p-2 bg-acce rounded-md mt-4 block">Rp. { packet.price }</button>
-
+            <button onClick={ () => {choosePacket(packet.price)} } className="md:text-2xl sm:text-md p-2 bg-acce rounded-md mt-4 block">Rp. { packet.price }</button>
         </div>
      );
 }
