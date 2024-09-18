c643dc4f061366a9981534df7a7ea3d55f2cd95e
garrychrstn <garrychrstn@gmail.com>
2024-09-18 14:28:28 +0700

    form works for data, but not yet for file

--- app/Models/Request.php
+++ app/Models/Request.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Request extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nama',
+        'email',
+        'alamat',
+        'notel',
+        'nik',
+        'ktp',
+        'password',
+        'coordinate'
+    ];
 }
